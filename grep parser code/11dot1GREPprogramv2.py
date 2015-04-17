2cf12558629d944200b4d8a3b4f205de6c7db806
unknown <902618@UK858954.uk.ema.ad.pwcinternal.com>
2015-04-17 17:04:05 +0100

    This version WORKS - so Exercise 11.1 is over, and has useful code snippets, now to work on File I/O

--- grep parser code/11dot1GREPprogramv2.py
+++ grep parser code/11dot1GREPprogramv2.py
@@ -10,38 +10,48 @@ def Openfile():
     print 'If you need a new file, enter \"new file\" when asked for a regex searh term'
     #ftext = fhand.read()
     SearchInputfunction(filename, fhand)
-    
+
+def CheckUserInput(userstring):
+    PosResoonses = ('Yes','yes','Y','y','yep')
+    if userstring in PosResoonses : return True
+    else: return False
+
+def Retryfunction(filename, fhand):
+    retrysamefile = raw_input('\nRetry another RegEx on the same file?: ')
+    if CheckUserInput(retrysamefile) : SearchInputfunction(filename,fhand)
+    else : 
+        retrydifffile =raw_input('\nRetry new file?')
+        if CheckUserInput(retrydifffile): Openfile()
+        else: 
+            print 'Thank you, shutting down'
+            raw_input()
+            quit()
+
+
+def Resultfunction(foundMatches2, inputtext, filename, fhand):                         #code to output to text file to be added later
+    if foundMatches2 == []: 
+        print 'No matches were found for %s in %s' % (inputtext, filename)
+        Retryfunction(filename,fhand)
+    else:
+        print "Result: " + str(foundMatches2)
+        Retryfunction(filename,fhand)
+
 def SearchInputfunction(filename,fhand):
     print '\n\nThe file we are searching is %s' % filename
     print '\nRemember you can check your Regular Expression at https://regex101.com/#python'
     inputtext = raw_input('\n\nEnter regex search term: ')
     if inputtext == 'new file' : Openfile()
-    else:
-        i = 0
+    else:    
+        foundMatches2 = []
+        fhand.seek(0)
         for line in fhand:
-            if i <20:
-                line = line.rstrip()
-                print '\n',line
-                print inputtext
+            foundMatches1 = re.findall(inputtext,line)
+            if foundMatches1 == []: continue
+            else:
+                foundMatches2.append(foundMatches1)
+        Resultfunction(foundMatches2, inputtext, filename, fhand)
 
-                foundRegex = re.search(inputtext, line,)
-                print foundRegex
-                i +=1
-            else: break
-            #print line
-            #inputwordlist = line.split()
-            #print inputwordlist
-            #delimiter = " "
-            #teststring = delimiter.join(inputwordlist)
-            #print teststring
-            # foundRegex = re.search(inputtext,teststring)
-            # if foundRegex != None:
-                # foundRegex = re.findall(inputtext,teststring)
-                # print "Result: " + str(foundRegex)
-            # else: 
-                # print '\n\nteststring\n\ninputtext'
-                # print "Search did not have any results."
-            
+
 def main():
     fhand = None
     Openfile()
